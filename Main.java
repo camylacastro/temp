8b33ce0f056c8411ffc0d8b4e466c28b9a83aed9
Camyla Castro <111926000+camylacastro@users.noreply.github.com>
2023-03-23 23:21:43 -0300

    Add files via upload
    by @acenelio

--- Main.java
+++ Main.java
@@ -9,16 +9,28 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double R, A, pi = 3.14159;
+		double A, B, C, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
 		
-		R = sc.nextDouble();
-		A = pi * R * R;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
 		
-		System.out.printf("A = %.4f%n", A);
+		TRIANGULO = A * C /2.0;
+		CIRCULO = 3.14159 * C * C;
+		TRAPEZIO = (A + B) /2.0 * C;
+		QUADRADO = B * B;
+		RETANGULO = A * B;
+		
+		System.out.printf("TRIANGULO : %.3f%n", TRIANGULO);
+		System.out.printf("CIRCULO : %.3f%n", CIRCULO);
+		System.out.printf("TRAPEZIO : %.3f%n", TRAPEZIO);
+		System.out.printf("QUADRADO : %.3f%n", QUADRADO);
+		System.out.printf("RETANGULO : %.3f%n", RETANGULO);
 		
 		
 		
 		
+
 		sc.close();
 	}
 
